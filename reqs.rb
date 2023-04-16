8aa94eb605038d9f6e5a5468d32775c506209ddd
a3laxi <mad.aalachi@gmail.com>
2023-04-16 22:53:10 +0200

    all good

--- reqs.rb
+++ reqs.rb
@@ -2,12 +2,16 @@ require 'net/http'
 
 if ARGV[0] == 'miror'
   url = URI.parse('http://127.0.0.1:4567/miror/'+ARGV[1])
+elsif ARGV[0] == 'count'
+  url = URI.parse('http://127.0.0.1:4567/count/'+ARGV[1])
+elsif ARGV[0] == 'do'
+  url = URI.parse('http://127.0.0.1:4567/do/'+ARGV[1])
 else
   url = URI.parse('http://127.0.0.1:4567/'+ARGV[0])
 end
 
-http = Net::HTTP.new(url.host, url.port)
 
+http = Net::HTTP.new(url.host, url.port)
 response = http.get(url.request_uri)
 
 puts response.body
